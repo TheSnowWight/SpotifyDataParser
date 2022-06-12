ddee3516066d2e4cfb21a230d5bcede47c67bff7
thesnowwight <45722564+TheSnowWight@users.noreply.github.com>
2022-06-12 14:49:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ Forked by: tsw
 """
 
 # Insert local parameters
+# Browsers can be unhappy about uncommon web ports
 
 username = 'your-spotify-username'
 client_id ='your-client-id'
 client_secret = 'your-client-secret'
-redirect_uri = 'http://localhost:6666/callback'
+redirect_uri = 'http://localhost:8080/callback'
 scope = 'user-read-recently-played'
